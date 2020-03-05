19ef6be4c96ff1dd7ceef7d856cbb1773a6a5f8c
RomanHavdo <roman.havdo@gmail.com>
2020-03-05 14:57:59 +0200

    Added last updates

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBlog } from "../../actions";
 
 class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return (
+        <img
+          src={
+            "https://my-test-node-bucket.s3.eu-west-3.amazonaws.com/" +
+            this.props.blog.imageUrl
+          }
+          alt="User's blog pic"
+        />
+      );
+    }
+  }
+
   render() {
     if (!this.props.blog) {
-      return '';
+      return "";
     }
 
     const { title, content } = this.props.blog;
@@ -18,6 +32,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,7 @@ module.exports = {
   mongoURI:
     "mongodb://RomanHav:uTNW3mxSD39SL9c8@cluster0-shard-00-00-szkfh.mongodb.net:27017,cluster0-shard-00-01-szkfh.mongodb.net:27017,cluster0-shard-00-02-szkfh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379"
+  redisUrl: "redis://127.0.0.1:6379",
+  accessKeyId: "AKIATQGBKLBMD5QHZFSS",
+  secretAccessKey: "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"
 };
